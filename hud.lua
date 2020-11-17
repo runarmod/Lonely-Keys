e0206d86c1bce86da02ca6528a527b8db62eb98c
Runar Saur Modahl <runarmod@gmail.com>
2020-11-17 01:48:23 +0100

    Added FPS to HUD

--- hud.lua
+++ hud.lua
@@ -8,17 +8,35 @@ function HUD:load()
     self.coin.img = love.graphics.newImage("assets/HUD/hud_coins.png")
     self.coin.height = self.coin.img:getHeight()
     self.coin.width = self.coin.img:getWidth()
-    self.coin.x = 50
+    self.coin.x = 300
     self.coin.y = 50
+
+    self.FPS = {}
+    self.FPS.x = 50
+    self.FPS.y = 50
 end
 
 function HUD:update(dt)
-    
+    HUD:updateFPS()
+end
+
+function HUD:updateFPS()
+    self.FPS.value = love.timer.getFPS()
 end
 
 function HUD:draw()
+    HUD:drawFPS()
     HUD:drawCoin()
     HUD:drawCoinAmount()
+end
+
+function HUD:drawFPS()
+    local FPSDisplayText = "FPS: " .. self.FPS.value
+    love.graphics.setFont(self.font)
+    love.graphics.setColor(0, 0, 0, 0.5)
+    love.graphics.print(FPSDisplayText, Map.camX + self.FPS.x + self.shadowOffset, Map.camY + self.FPS.y + self.shadowOffset)
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.print(FPSDisplayText, Map.camX + self.FPS.x, Map.camY + self.FPS.y)
 end
 
 function HUD:drawCoin()
