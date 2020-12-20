6a3252ff74d7fee6a6123e48f570b8055f7b6214
Runar Saur Modahl <runarmod@gmail.com>
2020-12-21 00:45:00 +0100

    Switched level 2 and 3

--- scores.lua
+++ scores.lua
@@ -1,24 +1,24 @@
 return {
     levels = {
         level2 = {
-            time = 1608506785,
-            value = 836
+            value = 836,
+            time = 1608506785
         },
         level1 = {
-            time = 1608503577,
-            value = 1009
+            value = 1009,
+            time = 1608503577
         },
         level4 = {
-            time = 1608506895,
-            value = 995
+            value = 995,
+            time = 1608506895
         },
         level3 = {
-            time = 1608506822,
-            value = 1051
+            value = 1051,
+            time = 1608506822
         },
         levelintro = {
-            time = 1608503544,
-            value = 1000
+            value = 1000,
+            time = 1608503544
         }
     }
 }
